13dd12ade7ec630e2199a09c78e490563888fc95
Manuel Vasquez <manuelvasquez@Manuels-MacBook-Air.local>
2019-05-10 19:16:00 -0400

    Saving project before updating angular version

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
-    <div class="app">
-      Hello!
+    <div class="app"> 
+    <button (click)="handleClick(username.value)">Change name</button>
+      <input 
+        type="text" #username>
+      <div>{{ name }}</div>
     </div>
   `
 })
-export class AppComponent {
-
+export class AppComponent{
+  title: string;
+  name: string = 'Manu';
+  handleClick(value: string){
+    console.log(value);
+  }
 }
